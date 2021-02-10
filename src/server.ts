d4f11d0e139c258b7153ccb2cfc983b4341f3a15
Yakov Danilichev <dan.yakov1105@mail.ru>
2021-02-10 21:52:34 +0300

    Add session and redis

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { RegisterResolver } from "./resolvers/user/Register";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
 
 (async () => {
   const app = express();
@@ -13,10 +17,37 @@ import { createConnection } from "typeorm";
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [RegisterResolver]
-    })
+    }),
+    context: ({ req }) => ({ req })
   });
 
   const PORT = process.env.PORT || 3000;
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis
+      }),
+      name: "bruh",
+      secret: "aslkdfjoiq12312",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+      }
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
